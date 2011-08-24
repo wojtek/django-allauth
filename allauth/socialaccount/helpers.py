2b11fb7e44bc374be17176b4e77203b1df78a1f4
Wojtek Podgorski <wojtek@superbright.org>
2011-08-24 13:00:56 +0200

    redirect

--- allauth/socialaccount/helpers.py
+++ allauth/socialaccount/helpers.py
@@ -109,7 +109,8 @@ def complete_social_login(request, data, account):
             messages.add_message \
             (request, messages.INFO, 
              _('The social account has been connected to your existing account'))
-            return HttpResponseRedirect(reverse('socialaccount_connections'))
+#            return HttpResponseRedirect(reverse('socialaccount_connections'))
+            return _login_social_account(request, account)
     else:
         if account.pk:
             # Login existing user
